6f0ada4dba6d2aac08901e7c82f3bdaf008ec2d4
Seadog1225 <aasssa1225@naver.com>
2020-06-16 11:29:50 +0900

    Updates

---
+++ precache-manifest.e6a4d8538e9b191068b9987f72768a19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df67d04d3241db5f2f45a801e9b166a8",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "261269a0404cfce9de69",
+    "url": "/movie_app_2020/static/css/main.49cf7d84.chunk.css"
+  },
+  {
+    "revision": "342738dfc01fea4968e4",
+    "url": "/movie_app_2020/static/js/2.f9a31e49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.f9a31e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "261269a0404cfce9de69",
+    "url": "/movie_app_2020/static/js/main.e7610b9b.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
