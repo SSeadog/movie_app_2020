0043e634587c7d129ca263124f5e2eefc225d348
Seadog1225 <aasssa1225@naver.com>
2020-06-18 18:02:21 +0900

    Updates

--- precache-manifest.e0fd7aa008cc06efeea0c356ece7ffaa.js
+++ precache-manifest.e0fd7aa008cc06efeea0c356ece7ffaa.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f0b49151a90488b05e6ca74eaa911cd6",
+    "revision": "3c8bee16eeb5e22059bf9c9d567b6db8",
     "url": "/movie_app_2020/index.html"
   },
   {
-    "revision": "3ee10a50be88ba36cdc2",
-    "url": "/movie_app_2020/static/css/main.7ec914ef.chunk.css"
+    "revision": "1874466e89b8db7d89f4",
+    "url": "/movie_app_2020/static/css/main.a50fbee8.chunk.css"
   },
   {
     "revision": "89018a9f4d0393aaa4a7",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie_app_2020/static/js/2.6fa82bcd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3ee10a50be88ba36cdc2",
-    "url": "/movie_app_2020/static/js/main.c8ea5cff.chunk.js"
+    "revision": "1874466e89b8db7d89f4",
+    "url": "/movie_app_2020/static/js/main.63591f8d.chunk.js"
   },
   {
     "revision": "86e4940ccaeed0f37e85",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_2020/precache-manifest.dceab0a3dc8033cabe435f5d36a0bb76.js"
+  "/movie_app_2020/precache-manifest.e0fd7aa008cc06efeea0c356ece7ffaa.js"
 );
 
 self.addEventListener('message', (event) => {
